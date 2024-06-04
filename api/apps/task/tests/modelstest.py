e9150eb9292fd2236e9518bcf33a58cd010821c8
Rhayane <Rhayanefreitas@hotmail.com>
2024-06-04 14:23:28 -0300

    Teste Criação de Super usuário, responsável e tarefa

---
+++ api/apps/task/tests/modelstest.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.contrib.auth.models import User as AuthUser
+from api.apps.task.models import TaskProfile
+from api.apps.task.models import user
+
+
+class TaskProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = AuthUser.objects.create_user(
+            username='testuser',
+            email='test@example.com',
+            password='testpassword'
+        )
+
+    def test_task_creation(self):
+        task = TaskProfile.objects.create(
+            title='Test Task created',
+            created_by=self.user
+        )
+        self.assertEqual(task.title, 'Test Task')
+        self.assertEqual(task.created_by, self.user)
+
+    def test_task_responsible_creation(self):
+        task = TaskProfile.objects.create(
+            title='Test Task created',
+            created_by=self.user
+        )
+        task_responsible = user.TaskResponsible.objects.create(
+            task=task,
+            user=self.user
+        )
+        self.assertEqual(task_responsible.task, task)
+        self.assertEqual(task_responsible.user, self.user)
