000c7bfbc66b5479e46e7b11764f47fadf0a87cb
Rhayane <Rhayanefreitas@hotmail.com>
2024-06-14 14:12:47 -0300

    Melhorias página Admin

--- api/apps/task/admin.py
+++ api/apps/task/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin 
+
 from api.apps.task.models import task
+
+from django.utils.translation import gettext_lazy as _
+
+# Personalizando a variável de custo dos conjuntos de campos
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = ( 
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    
+# Personalizando a página para alterar usuários.
+
+
 
 admin.site.register(task.TaskProfile)
 admin.site.register(task.TaskResponsible)
-admin.site.register(task.User)
+admin.site.register(task.User, UserAdmin)
